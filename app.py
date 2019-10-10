23c045aa9e3296f6b64de45461777b0212654a61
mine444 <51392663+mine444@users.noreply.github.com>
2019-10-10 15:30:42 +0700

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def callback():
     elif (userText == 'ทำไรดี') :
         sendText(user,'ทำบุญก็ดีนะ')
     else :
-        sendText(,'ไม่รู้')   
+        sendText(user,'ไม่รู้')   
     return '',200
 
   def sendText(user, text):
